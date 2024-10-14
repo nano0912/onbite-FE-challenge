db64d231c7250546c9d1dddda60630b37733c734
hyojeong <nano0912d@gmail.com>
2024-10-14 14:41:05 +0900

    update: day15 mission #10

--- onebite-cinema-app/src/app/(with-searchbar)/page.tsx
+++ onebite-cinema-app/src/app/(with-searchbar)/page.tsx
@@ -1,6 +1,8 @@
 import MovieItem from '@/components/movie-item';
 import style from './page.module.css';
 import { MovieData } from '@/types';
+import { Suspense } from 'react';
+import MovieListSkeleton from '@/components/skeleton/movie-list-skeleton';
 
 async function RecoMovies() {
   const response = await fetch(
@@ -40,19 +42,25 @@ async function AllMovies() {
   );
 }
 
-export default async function Home() {
+export const dynamic = 'force-dynamic';
+
+export default function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 가장 추천하는 영화</h3>
         <div className={style.reco_container}>
-          <RecoMovies />
+          <Suspense fallback={<MovieListSkeleton count={3} />}>
+            <RecoMovies />
+          </Suspense>
         </div>
       </section>
       <section>
         <h3>등록된 모든 영화</h3>
         <div className={style.all_container}>
-          <AllMovies />
+          <Suspense fallback={<MovieListSkeleton count={20} />}>
+            <AllMovies />
+          </Suspense>
         </div>
       </section>
     </div>
